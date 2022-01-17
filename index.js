1eec7d65f01133de8e3ab7a782b2bdb46861ea1c
Caloy9999 <97885002+Caloy9999@users.noreply.github.com>
2022-01-17 19:08:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const axios = require('axios')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const coinId = "vigorus";
+//const clientId = "932579782628106281";
+const guildId = "932575805316427786";
+const botSecret = "OTMyNTc5NzgyNjI4MTA2Mjgx.YeVCmg.ep6p5KNwGSa6PK6L049F_KFEhGk";
+
 function getPrices() {
 
 
 	// API for price data.
-	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${process.env.PREFERRED_CURRENCY}&ids=${process.env.COIN_ID}`).then(res => {
+	axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${coinId}`).then(res => {
 		// If we got a valid response
 		if(res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h) {
 			let currentPrice = res.data[0].current_price || 0 // Default to zero
 			let priceChange = res.data[0].price_change_percentage_24h || 0 // Default to zero
 			let symbol = res.data[0].symbol || '?' 
+			
 			client.user.setPresence({
 				game: {
 					// Example: "Watching -5,52% | BTC"
@@ -20,10 +26,12 @@ function getPrices() {
 					type: 3 // Use activity type 3 which is "Watching"
 				}
 			})
-
+			
+			
+			console.log('Updated price to', currentPrice)
 			client.guilds.find(guild => guild.id === process.env.SERVER_ID).me.setNickname(`${(currentPrice).toLocaleString().replace(/,/g,process.env.THOUSAND_SEPARATOR)}${process.env.CURRENCY_SYMBOL}`)
 
-			console.log('Updated price to', currentPrice)
+		
 		}
 		else
 			console.log('Could not load player count data for', process.env.COIN_ID)
@@ -40,5 +48,5 @@ client.on('ready', () => {
 	setInterval(getPrices, Math.max(1, process.env.MC_PING_FREQUENCY || 1) * 60 * 1000)
 })
 
-// Login to Discord
+//https://discord.com/oauth2/authorize?client_id=932579782628106281&permissions=0&scope=bot%20applications.commands
 client.login(process.env.DISCORD_TOKEN)
